98e5c68df9a03ad4c6df822b7b264aa1dbc99425
Anunnaki <92318629+AliTigui@users.noreply.github.com>
2024-08-26 20:42:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
-
-
+import sqlite3
 import asyncio
 import discord
 from instagrapi import Client # type: ignore
 cl = Client()
-cl.login("Username", "password")
-import sqlite3
+cl.login("username", "password")
+
 
 con = sqlite3.connect("server.db")
 cur = con.cursor()
@@ -15,31 +14,31 @@ class MyClient(discord.Client):
         while True:
             for k in self.guilds:
                 g=self.get_guild(k.id)
+                
                 res = cur.execute(f"SELECT channel_id FROM servers WHERE server_id = ?",(g.id,))
                 try:
+                    
                     ch=int(res.fetchone()[0])
-                    
-                except:
-                    continue
-                channel = g.get_channel(ch)
-                pages = cur.execute("SELECT page FROM pages WHERE server_id = ?",(g.id,))
-                pages= list(map(lambda e: e[0],pages))
-                for page in pages:
-                    await asyncio.sleep(60) 
-                    
-                    try:
+                    channel = (g.get_channel(ch) or await g.fetch_channel(ch))
+                   
+                    pages = cur.execute("SELECT page FROM pages WHERE server_id = ?",(g.id,))
+                    pages= list(map(lambda e: e[0],pages))
+                    for page in pages:
+                        await asyncio.sleep(60) 
                         user_id = cl.user_id_from_username(page)
-                        code = cl.user_medias(user_id, 1)[0].code.strip()
-                    except:
-                        continue
-                    codes = cur.execute("SELECT post_code FROM posted WHERE server_id = ?",(g.id,))
-                    codes = list(map(lambda e: e[0],codes))
-                
-                    if not( code in codes):
-                        await channel.send("https://www.instagram.com/p/"+code)
-                        cur.execute("INSERT INTO posted (server_id,post_code) VALUES(?,?)",(g.id,code))
-                        con.commit()
-            
+                        code = cl.user_medias(user_id, 1)[0].code
+                        codes = cur.execute("SELECT post_code FROM posted WHERE server_id = ?",(g.id,))
+                        codes = list(map(lambda e: e[0],codes))
+                        if not( code in codes):  
+                            print("hope it get sent")
+                            await channel.send("https://www.instagram.com/p/"+code)
+                            print("message sent")
+                            cur.execute("INSERT INTO posted (server_id,post_code) VALUES(?,?)",(g.id,code))
+                            con.commit()
+                        else:
+                            print("no new post")
+                except:    
+                    pass
  
     async def on_ready(self):
 
@@ -51,7 +50,7 @@ class MyClient(discord.Client):
         self.server=int(message.guild.id)
         if message.author.id == self.user.id:
             return
-        if message.content.startswith('!news') and message.author.guild_permissions.ban_members:
+        if message.content.startswith('!news') and (message.author.guild_permissions.ban_members or message.author.id ==1231205487555645450):
             res = cur.execute("SELECT * FROM servers WHERE server_id  = ?",(self.server,))
             if res.fetchone() is None:
                 cur.execute("INSERT INTO servers VALUES (?,?)",(self.server,self.channel))
@@ -59,17 +58,16 @@ class MyClient(discord.Client):
             else :
                 cur.execute("UPDATE servers SET channel_id = ? WHERE server_id   = ?",(self.channel,self.server))
                 con.commit()
-            
-        if message.content.startswith('!add_insg') and message.author.guild_permissions.ban_members:
+            await message.reply("channel set")
+        if message.content.startswith('!add_insg') and (message.author.guild_permissions.ban_members or message.author.id ==1231205487555645450):
             in_page = message.content.split()[1].strip()
             res = cur.execute("SELECT * FROM pages WHERE server_id = ? AND page= ?",(self.server,in_page))
             if res.fetchone() is None:
                 print( in_page)
                 cur.execute("INSERT INTO pages (server_id,page) VALUES (?,?)",(self.server,in_page))
                 con.commit()
-            
-
-
+            await message.reply("page added")
+   
             
 
             
@@ -80,4 +78,4 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
 
-client.run('your token')
+client.run('token')
